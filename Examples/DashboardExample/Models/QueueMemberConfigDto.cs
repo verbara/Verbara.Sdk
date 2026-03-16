304412c306e951208d5d852880fec7f962affbd4
Harol A. Reina H. <hreina@verbara.io>
2026-03-16 13:42:27 -0500

    feat(queue-config): add QueueConfigDto and QueueMemberConfigDto record models

---
+++ Examples/DashboardExample/Models/QueueMemberConfigDto.cs
@@ -0,0 +1,12 @@
+namespace DashboardExample.Models;
+
+public sealed record QueueMemberConfigDto
+{
+    public int Id { get; set; }
+    public int QueueConfigId { get; set; }
+    public string Interface { get; set; } = "";
+    public string? MemberName { get; set; }
+    public string? StateInterface { get; set; }
+    public int Penalty { get; set; }
+    public int Paused { get; set; }
+}
