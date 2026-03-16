61b7f26c2abf1267fcbf1867de8d3698db3817ea
Harol A. Reina H. <hreina@verbara.io>
2026-03-16 09:13:35 -0500

    feat(routes): add Sprint 5 route and time condition model types

---
+++ Examples/DashboardExample/Models/RouteTrunk.cs
@@ -0,0 +1,8 @@
+namespace DashboardExample.Models;
+
+public sealed class RouteTrunk
+{
+    public string TrunkName { get; set; } = "";
+    public string TrunkTechnology { get; set; } = "";
+    public int Sequence { get; set; }
+}
---
+++ Examples/DashboardExample/Models/TimeConditionConfig.cs
@@ -0,0 +1,15 @@
+namespace DashboardExample.Models;
+
+public sealed class TimeConditionConfig
+{
+    public int Id { get; set; }
+    public string ServerId { get; set; } = "";
+    public string Name { get; set; } = "";
+    public string MatchDestType { get; set; } = "";
+    public string MatchDest { get; set; } = "";
+    public string NoMatchDestType { get; set; } = "";
+    public string NoMatchDest { get; set; } = "";
+    public bool Enabled { get; set; } = true;
+    public List<TimeRangeEntry> Ranges { get; set; } = [];
+    public List<HolidayEntry> Holidays { get; set; } = [];
+}
