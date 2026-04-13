2dbb6c5bffbc53b86a795218ea53e31d0e54495b
Harol A. Reina H. <hreina@verbara.io>
2026-04-13 11:13:30 -0500

    fix(test-infra): add FunctionalTestFixture xunit wrapper for collection lifecycle

--- Tests/Asterisk.Sdk.FunctionalTests/Infrastructure/Collections/FunctionalCollection.cs
+++ Tests/Asterisk.Sdk.FunctionalTests/Infrastructure/Collections/FunctionalCollection.cs
@@ -1,8 +1,7 @@
 using Asterisk.Sdk.FunctionalTests.Infrastructure.Fixtures;
-using Asterisk.Sdk.TestInfrastructure.Stacks;
 
 namespace Asterisk.Sdk.FunctionalTests.Infrastructure.Collections;
 
 [System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1711:Identifiers should not have incorrect suffix", Justification = "xunit collection definition class must end in 'Collection'")]
 [CollectionDefinition("Functional")]
-public sealed class FunctionalCollection : ICollectionFixture<FunctionalFixture>, ICollectionFixture<ToxiproxyFixture>;
+public sealed class FunctionalCollection : ICollectionFixture<FunctionalTestFixture>, ICollectionFixture<ToxiproxyFixture>;
